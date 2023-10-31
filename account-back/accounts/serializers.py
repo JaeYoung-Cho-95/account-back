ab1b7e7f6c4b375f080a17de5265f13e95afa3b3
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-10-31 14:04:59 +0900

    add. signup

---
+++ account-back/accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = [
+            "pk",
+            "username",
+            
+            ]
+    
