de69f4412e79357a61ad9b8625bdc3c3e9c7dfe3
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-10-31 14:01:26 +0900

    add : sigup

--- account-back/A/settings.py
+++ account-back/A/settings.py
@@ -34,6 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local app
+    "accounts",
+    
+    # third patry
+    "rest_framework",
+    
+    # django main app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
