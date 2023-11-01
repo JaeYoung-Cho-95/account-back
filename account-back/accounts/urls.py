211f98dda9c1fedb0691c53335a870350122efec
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-11-01 21:13:53 +0900

    add. aws rds settings

--- account-back/accounts/urls.py
+++ account-back/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import UserCreateView
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
+    # path('', include('dj_rest_auth.urls')),
     path('signup/', UserCreateView.as_view()),
 ]
