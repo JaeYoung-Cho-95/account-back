60c3c8a4fe39bf836345f349cbe755af2233425b
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-11-08 14:12:42 +0900

    test. create To delete / correct and uncorrect user

---
+++ account-back/tests/accounts/test_correct_createToDelete.py
@@ -0,0 +1,69 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import requests
+import random
+
+signup_url = "http://localhost:8000/accounts/signup/"
+login_url = "http://localhost:8000/accounts/login/"
+detail_url = "http://localhost:8000/accounts/detail/"
+
+n = random.randrange(999)
+signup_data = {
+    "email": f"{n}@gmail.com",
+    "username": f"t_{n}",
+    "nickname": f"nick_{n}",
+    "password": f"aA!123{n}",
+}
+login_data = {
+    "email": f"{n}@gmail.com",
+    "password": f"aA!123{n}"
+}
+
+change_data = {
+    "email": f"{n}@gmail.com",
+    "nickname": f"{n}",
+    "password": "A1q2w3e4r!!"
+}
+delete_data = {
+    "email": f"{n}@gmail.com",
+    "password": f"A1q2w3e4r!!"
+}
+
+
+def test_correct_create():
+    response = requests.post(signup_url, signup_data)
+
+    assert response.status_code == 201
+
+def test_exsist_user_login():
+    response = requests.post(url=login_url, data=login_data)
+    
+    assert response.status_code == 200
+
+def test_correct_user_detail():
+    response = requests.post(url=login_url, data=login_data)
+    
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+    data = {
+        'email': login_data["email"]
+    }
+    response = requests.get(url=detail_url, headers=headers, params=data)
+
+    assert response.json()["email"] == login_data["email"]
+    assert response.status_code == 200
+
+def test_exsist_user_change():
+    response = requests.post(url=login_url, data=login_data)
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+    
+    response = requests.patch(url=detail_url, data=change_data, headers=headers)
+    
+    assert response.status_code == 200
+    
+def test_exsist_user_delete():
+    response = requests.post(url=login_url, data=delete_data)
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+    response = requests.delete(url=detail_url, data=delete_data, headers=headers)
+    
+    assert response.status_code == 204
---
+++ account-back/tests/accounts/test_uncorrect_createToDeletedetail.py
@@ -0,0 +1,69 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import requests
+import random
+from secret import duplicated_user_signup, duplicated_user_login
+
+signup_url = "http://localhost:8000/accounts/signup/"
+login_url = "http://localhost:8000/accounts/login/"
+detail_url = "http://localhost:8000/accounts/detail/"
+
+
+def test_uncorrect_create():
+    data = duplicated_user_signup
+    response = requests.post(signup_url, data)
+
+    assert response.status_code == 400
+    
+def test_noexsist_user_login():
+    n = random.randrange(1000)
+    data = {"email": f"{n}@gmail.com", "password": f"{n}"}
+    response = requests.post(url=login_url, data=data)
+
+    assert response.status_code == 400
+
+def test_uncorrect_user_detail():
+    n = random.randrange(1000)
+    data = {"email": f"{n}@gmail.com", "password": f"{n}"}
+    response = requests.post(detail_url, data)
+
+    assert response.status_code == 401
+
+def test_noexsist_user_change():
+    change_data = {
+    "email": f"@gmail.com",
+    "nickname": f"",
+    "password": "A1q!w3@!"
+    }
+    
+    response = requests.patch(url=detail_url, data=change_data)
+    assert response.status_code == 401
+    
+    response = requests.post(url=login_url, data=duplicated_user_login)
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+
+    response = requests.patch(url=detail_url, data=change_data, headers=headers)
+    
+    assert response.status_code == 400
+
+def test_exsist_user_delete():
+    response = requests.delete(url=detail_url, data=duplicated_user_login)
+    assert response.status_code == 401
+    
+    response = requests.post(url=login_url, data=duplicated_user_login)
+    duplicated_user_login["email"] = "333@gmail.com"
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+    response = requests.delete(url=detail_url, data=duplicated_user_login, headers=headers)
+    
+    assert response.status_code == 500
+    
+    response = requests.post(url=login_url, data=duplicated_user_login)
+    duplicated_user_login["password"] = "1234qqqq"
+    headers = {"Authorization": f"Bearer {response.json()['jwt_token']['access_token']}"}
+    response = requests.delete(url=detail_url, data=duplicated_user_login, headers=headers)
+    assert response.status_code == 401
+    
+    
+    
+    
