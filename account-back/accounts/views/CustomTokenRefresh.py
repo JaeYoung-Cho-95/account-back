11f714ea4f13c2c3a63601cea62a85652c7b3888
JaeYoung-Cho-95 <whwo9745@gmail.com>
2023-11-12 12:12:40 +0000

    modify. user doesn't have access token logout our service, request.user object is annoymouse. so change this logic in CutomTokenRefresh view

--- account-back/accounts/views/CustomTokenRefresh.py
+++ account-back/accounts/views/CustomTokenRefresh.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
 import logging
 
 logger = logging.getLogger("A")
@@ -13,9 +14,10 @@ class CustomTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
         # 쿠키에서 refresh token 가지고 오기
         refresh_token = request.COOKIES.get("refresh_token")
-
+    
         # 각종 사용자 및 토큰 정보 인증
         response, valid_res = self.valid_token(refresh_token, request)
+        User = get_user_model()
         
         # 인증 실패시 에러 메시지 및 상태코드 응답
         if not valid_res:
@@ -23,12 +25,17 @@ class CustomTokenRefreshView(TokenRefreshView):
         
         # 기존 refresh token 을 이용해서 access_token 발급
         refresh_token = RefreshToken(refresh_token)
-        data = {"access": f"{refresh_token.access_token}"}
+        user = refresh_token["user_id"]
+        
+        # 새로운 refresh token 발급
+        new_refresh_token = RefreshToken.for_user(User.objects.get(id=user))
+        
+        # refresh token 으로 access token 발급
+        data = {"access_token": f"{new_refresh_token.access_token}"}
+        
         # 기존 refresh token 은 blacklist 에 추가
         refresh_token.blacklist()
-
-        # 새로운 refresh token 발급
-        new_refresh_token = RefreshToken.for_user(request.user)
+        
         response = Response(data=data, status=status.HTTP_200_OK)
         response.set_cookie(
             "refresh_token", f"{new_refresh_token}", httponly=True, secure=True
@@ -60,14 +67,4 @@ class CustomTokenRefreshView(TokenRefreshView):
                 ),
                 False,
             )
-
-        if isinstance(request.user, AnonymousUser):
-            return (
-                Response(
-                    {"message": "로그인이 필요합니다."},
-                    status=status.HTTP_401_UNAUTHORIZED,
-                ),
-                False,
-            )
-
         return (None, True)
